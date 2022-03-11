9ecfad0c8648324a5a298c4d92fd2266b8fcc265
wolfie <idkwhydoyoucare123@gmail.com>
2022-03-11 16:21:36 -0600

    Base dynamic pool entry class

--- src/main/java/me/coley/cafedude/classfile/constant/CpInvokeDynamic.java
+++ src/main/java/me/coley/cafedude/classfile/constant/CpInvokeDynamic.java
@@ -5,11 +5,9 @@ package me.coley.cafedude.classfile.constant;
  * and a bootstrap method index in the class's bootstrap-methods attribute.
  *
  * @author Matt Coley
+ * @author Wolfie / win32kbase
  */
-public class CpInvokeDynamic extends ConstPoolEntry {
-	private int bsmIndex;
-	private int nameTypeIndex;
-
+public class CpInvokeDynamic extends ConstDynamic {
 	/**
 	 * @param bsmIndex
 	 * 		Index in the class's bootstrap method attribute-table.
@@ -17,38 +15,6 @@ public class CpInvokeDynamic extends ConstPoolEntry {
 	 * 		Index of {@link CpNameType} in pool.
 	 */
 	public CpInvokeDynamic(int bsmIndex, int nameTypeIndex) {
-		super(INVOKE_DYNAMIC);
-		this.bsmIndex = bsmIndex;
-		this.nameTypeIndex = nameTypeIndex;
-	}
-
-	/**
-	 * @return Index in the class's bootstrap method attribute-table.
-	 */
-	public int getBsmIndex() {
-		return bsmIndex;
-	}
-
-	/**
-	 * @param bsmIndex
-	 * 		New index in the class's bootstrap method attribute-table.
-	 */
-	public void setBsmIndex(int bsmIndex) {
-		this.bsmIndex = bsmIndex;
-	}
-
-	/**
-	 * @return Index of {@link CpNameType} in pool.
-	 */
-	public int getNameTypeIndex() {
-		return nameTypeIndex;
-	}
-
-	/**
-	 * @param nameTypeIndex
-	 * 		New index of {@link CpNameType} in pool.
-	 */
-	public void setNameTypeIndex(int nameTypeIndex) {
-		this.nameTypeIndex = nameTypeIndex;
+		super(INVOKE_DYNAMIC, bsmIndex, nameTypeIndex);
 	}
 }
