012eec1b4cb47ec483c9d2c11bf5cf03fe19d924
Matt <mcoley2@gmu.edu>
2025-07-04 17:07:47 -0400

    Add missing internal attributes to AttributeVersions lookup

--- core/src/main/java/software/coley/cafedude/classfile/attribute/AttributeVersions.java
+++ core/src/main/java/software/coley/cafedude/classfile/attribute/AttributeVersions.java
@@ -1,8 +1,7 @@
 package software.coley.cafedude.classfile.attribute;
 
-import software.coley.cafedude.classfile.VersionConstants;
-
 import jakarta.annotation.Nonnull;
+import software.coley.cafedude.classfile.VersionConstants;
 
 /**
  * Attribute relations to class file versions.
@@ -21,7 +20,7 @@ public class AttributeVersions implements AttributeConstants, VersionConstants {
 	 * If the attribute's introduction version is unknown, then {@code -1}.
 	 */
 	public static int getIntroducedVersion(@Nonnull String attributeName) {
-		switch (attributeName) {
+		return switch (attributeName) {
 			case CODE:
 			case CONSTANT_VALUE:
 			case DEPRECATED:
@@ -31,7 +30,7 @@ public class AttributeVersions implements AttributeConstants, VersionConstants {
 			case LOCAL_VARIABLE_TABLE:
 			case SOURCE_FILE:
 			case SYNTHETIC:
-				return JAVA1;
+				yield JAVA1;
 			case ANNOTATION_DEFAULT:
 			case ENCLOSING_METHOD:
 			case LOCAL_VARIABLE_TYPE_TABLE:
@@ -41,39 +40,36 @@ public class AttributeVersions implements AttributeConstants, VersionConstants {
 			case RUNTIME_VISIBLE_PARAMETER_ANNOTATIONS:
 			case SIGNATURE:
 			case SOURCE_DEBUG_EXTENSION:
-				return JAVA5;
+				yield JAVA5;
 			case STACK_MAP_TABLE:
-				return JAVA6;
+				yield JAVA6;
+			case CHARACTER_RANGE_TABLE:
+			case COMPILATION_ID:
+			case SOURCE_ID:
 			case BOOTSTRAP_METHODS:
-				return JAVA7;
+				yield JAVA7;
 			case METHOD_PARAMETERS:
 			case RUNTIME_INVISIBLE_TYPE_ANNOTATIONS:
 			case RUNTIME_VISIBLE_TYPE_ANNOTATIONS:
-				return JAVA8;
+				yield JAVA8;
 			case MODULE:
 			case MODULE_MAIN_CLASS:
 			case MODULE_PACKAGES:
-				return JAVA9;
+			case MODULE_HASHES:
+			case MODULE_RESOLUTION:
+			case MODULE_TARGET:
+				yield JAVA9;
 			case NEST_HOST:
 			case NEST_MEMBERS:
-				return JAVA11;
+				yield JAVA11;
 			case RECORD:
 				// Records first preview in 14
-				return JAVA14;
+				yield JAVA14;
 			case PERMITTED_SUBCLASSES:
 				// Sealed classes first preview in 15
-				return JAVA15;
+				yield JAVA15;
 			default:
-				break;
-		}
-		// TODO: Research unused attributes?
-		//  - Some of the following items are listed as consts in the compiler, but are nowhere in the spec...
-		//     - CHARACTER_RANGE_TABLE
-		//     - COMPILATION_ID
-		//     - MODULE_HASHES
-		//     - MODULE_RESOLUTION
-		//     - MODULE_TARGET
-		//     - SOURCE_ID
-		return -1;
+				yield -1;
+		};
 	}
 }
